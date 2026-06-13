609dd351069146a0c0748acad2e458efc1f716cb
todayisstormy <todayiswendy07@gmail.com>
2026-06-13 10:49:30 +0330

    fix : issue #3 Arad and I created the normal agentBehavior and it's interface

---
+++ LastSugarFaz6/src/Interfaces/IBehavior.java
@@ -0,0 +1,27 @@
+package Interfaces;
+
+import Models.Agent;
+
+public interface IBehavior {
+
+    public void survival(Agent agent, ISpaceProvider space);
+
+    public void reproductionInherit(Agent agent);
+
+    public boolean canBeParent(Agent agent);
+
+    public  double getWelfare(Agent agent, double w1, double w2);
+
+    public  double getMRS(Agent agent, double w1, double w2);
+
+    public boolean canBeLender(Agent agent);
+
+    public int requiredSpiceAmount(Agent agent);
+
+    public int requiredSugarAmount(Agent agent);
+
+    public boolean needsSpice(Agent agent);
+
+    public boolean needsSugar(Agent agent);
+
+}
